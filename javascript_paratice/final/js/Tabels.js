67fcd0ae6590c9b32ba54e9ecbd0c1d51ee5367e
Divyaagowd <117075103+Divyaagowd@users.noreply.github.com>
2022-11-27 14:39:50 +0530

    Add files via upload
    uploaded js file

---
+++ javascript_paratice/final/js/Tabels.js
@@ -0,0 +1,18 @@
+function output(){
+    var a=parseInt(prompt("Enter a table value"));
+    var n=prompt("enter length");
+    var m=typeof(a);
+    console.log(typeof(a));
+    if (m == 'number'){
+        if(a<=1){
+            alert("Enter greater than zero value");
+        }
+        else{
+            var m=0;
+            for(i=1; i<=n; i++){
+                var m=m+a;
+                console.log(m);
+            }
+        }
+    }
+}
