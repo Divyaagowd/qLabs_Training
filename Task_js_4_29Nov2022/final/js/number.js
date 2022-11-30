f9b2e7ddedbba7a03d78ed434d59fba684fd3a0f
Divyaagowd <117075103+Divyaagowd@users.noreply.github.com>
2022-11-30 08:37:45 +0530

    Add files via upload

--- Task_js_4_29Nov2022/final/js/number.js
+++ Task_js_4_29Nov2022/final/js/number.js
@@ -1,56 +1,58 @@
 function calculate() {
-    var a=0
-    res = 0;
-    n = parseInt(document.getElementById("enter").value);
-    b = n;
-    var total=" ";
-   while (n != 0) {
+  var total="";
+     var res = 0;
+     var a=0;
+    n = (document.getElementById("enter").value);
      var length= n.length;
-     for(var i=0; i<=length; i++){
-
-     }
-      
+    //  console.log(length);
+     for(var i=0; i<length; i++)
+     {
+      var a=parseInt(n[i]);
+      // console.log(parseInt(a));
       switch (a) {
         case 0:
-          res = "0";
+          res = "zero";
           break;
         case 1:
-          res ="1";
+          res ="one";
           break;
         case 2:
-          res = "2";
+          res = "two";
           break;
         case 3:
-          res = "3";
+          res = "three";
           break;
         case 4:
-          res =  "4";
+          res =  "four";
           break;
         case 5:
-          res = "5";
+          res = "five";
           break;
         case 6:
-          res ="6";
+          res ="six";
           break;
         case 7:
-          res ="7";
+          res ="seven";
           break;
         case 8:
-          res = "8";
+          res = "eight";
           break;
         case 9:
-          res = "9";
+          res = "nine";
           break;
         case 10:
-          res = "10";
+          res = "ten";
           break;
-        default:
-          res = "not a valid number";
-          break;
+        
       }
-          total=res+" "+total;
-          console.log(total);
-    }
-    document.getElementById("number").innerHTML=total;
-  }
+      total=total+" "+res;
+      document.getElementById("number").innerHTML=n;
+      document.getElementById("res").innerHTML=total;
+      }
+          
+       
+    
+   
+}
+
   
